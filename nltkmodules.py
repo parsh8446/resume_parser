932e65bd931d3d0d7b54ed2325ccf29adefcb37b
Parashuram Chavan <61647862+parsh8446@users.noreply.github.com>
2022-05-30 19:06:01 +0530

    Create nltkmodules.py

---
+++ nltkmodules.py
@@ -0,0 +1,9 @@
+import nltk
+nltk.download('omw-1.4')
+nltk.download('averaged_perceptron_tagger')
+nltk.download('stopwords')
+nltk.download('punkt')
+nltk.download('wordnet')
+nltk.download('maxent_ne_chunker')
+nltk.download('words')
+stop_words = stopwords.words('english')
